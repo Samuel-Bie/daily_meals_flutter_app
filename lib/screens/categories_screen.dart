3d2efd3d797dd630f4398f7bc45238b84e182e86
Samuel Bie <samuelbiezinho@gmail.com>
2020-07-28 13:45:55 +0200

    Add tabs

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,25 +6,20 @@ import 'package:daily_meals/widgets/category_item_widget.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SamMeals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: <Widget>[
-          ...DUMMY_CATEGORIES.map((e) {
-            return CategoryItem(
-              category: e,
-            );
-          }).toList()
-        ],
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: <Widget>[
+        ...DUMMY_CATEGORIES.map((e) {
+          return CategoryItem(
+            category: e,
+          );
+        }).toList()
+      ],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import 'package:daily_meals/screens/categories_screen.dart';
+import 'package:daily_meals/screens/favorites_screen.dart';
+import 'package:daily_meals/widgets/main_drawer_widget.dart';
+
+class TabsScreen extends StatefulWidget {
+  TabsScreen({Key key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return buildTabOntheBottom(context);
+  }
+
+  int _selectedPageIndex = 0;
+  List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories'
+    }, {
+      'page': FavoritesScreen(),
+      'title': 'Favorites'
+    },
+  ];
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  Widget buildTabOntheBottom(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( _pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
+        onTap: _selectPage,
+        items: [
+          BottomNavigationBarItem(
+            //  backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTabOntheTop(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.category), text: 'Categories'),
+            Tab(icon: Icon(Icons.favorite), text: 'Favorites'),
+          ]),
+        ),
+        body: TabBarView(children: [
+          CategoriesScreen(),
+          FavoritesScreen(),
+        ]),
+      ),
+    );
+  }
+}
